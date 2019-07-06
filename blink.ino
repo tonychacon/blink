0ae9d4f5e7e4c0f74e68d1a303a8b9d322505216
LitvinchukAndrey <acethecreator111@gmail.com>
2019-07-06 23:45:41 +0300

    'Lol'

--- blink.ino
+++ blink.ino
@@ -18,7 +18,13 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+<<<<<<< HEAD
   delay(3000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(5000);               // wait for a second
+=======
+  delay(1000);               // wait for a second
+  igitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+>>>>>>> better
 }
