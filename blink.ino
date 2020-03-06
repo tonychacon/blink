962c474fa09b76bb282b650d1d00d8e4d46e2814
Alienn-Know <48494466+Alienn-Know@users.noreply.github.com>
2020-03-06 21:40:37 +0300

    коммит слияния

--- blink.ino
+++ blink.ino
@@ -22,4 +22,4 @@ void loop() {
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);               // wait for a second
 }
-// Изменение в файле добавленны
+// Изменение в файле удалены
